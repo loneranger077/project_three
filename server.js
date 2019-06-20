7200a15c81746ecc503517df2a2d9cc3d24f96aa
Joel Gaeta <joelsgaeta@gmail.com>
2019-06-20 12:38:37 -0400

    adds scripts to package.json for starting servers in dev mode

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+);
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,8 +20,11 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+if (process.env.NODE_ENV === "production") {
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/public/index.html"));
+  });
+}
 
 // Start the API server
 app.listen(PORT, function() {
