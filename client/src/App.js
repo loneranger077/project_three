9bf019c8f193f72d87ad1c0635750f037b52251f
tlavette <tlavette@yahoo.com>
2019-06-25 13:58:18 -0400

    framework for app and login page created. Routes added with react router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Container from "./Components/Container";
+import AppPage from "./Components/AppPage";
 
 function App() {
-  return <Container />;
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={AppPage} />
+        <Route path="/login" component={Container} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/Components/AppPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppPage (props) {
+    return "App Page";
+}
+
+export default AppPage;
